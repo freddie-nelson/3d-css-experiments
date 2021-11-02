1658d0570ee31d9ddf2b51707a6e346228a3dbb1
Freddie Nelson <freddie0208@hotmail.com>
2021-11-02 16:15:25 +0000

    sass

--- js/cuboid.js
+++ js/cuboid.js
@@ -18,92 +18,28 @@ class Cuboid extends HTMLElement {
 
     const texture = this.hasAttribute("texture") ? this.getAttribute("texture") : "rgba(0, 0, 0, 0.2)";
 
-    const cssVariables = `
-      --width: ${width}vmin;
-      --height: ${height}vmin;
-      --depth: ${depth}vmin;
+    const cssVariables = document.createElement("style");
+    cssVariables.textContent = `
+      :host {
+        --width: ${width}vmin;
+        --height: ${height}vmin;
+        --depth: ${depth}vmin;
 
-      --x: ${x}vmin;
-      --y: ${y}vmin;
-      --z: ${z}vmin;
+        --x: ${x}vmin;
+        --y: ${y}vmin;
+        --z: ${z}vmin;
 
-      --rotate-x: ${rotateX}deg;
-      --rotate-y: ${rotateY}deg;
-      --rotate-z: ${rotateZ}deg;
+        --rotate-x: ${rotateX}deg;
+        --rotate-y: ${rotateY}deg;
+        --rotate-z: ${rotateZ}deg;
 
-      --background: ${texture};
-    `;
-
-    const style = document.createElement("style");
-    style.textContent = `
-      .cuboid {
-        ${cssVariables}
-
-        position: absolute;
-        transform-style: preserve-3d;
-        transform: translate3d(var(--x), var(--y), var(--z))
-                   rotateX(var(--rotate-x)) 
-                   rotateY(var(--rotate-y)) 
-                   rotateZ(var(--rotate-z));
-
-        height: var(--depth);
-        width: var(--width);
-
-        /* background-color: rgba(255, 0, 180, 0.3); */
-      }   
-      
-      .cuboid__face {
-        background: var(--background);
-        position: absolute;
-        transform-origin: 50% 50%;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-      }
-
-      /* Front face */
-      .cuboid__face:nth-of-type(1) {
-        height: var(--height);
-        width: var(--width);
-        transform: translate(-50%, -50%) rotateX(-90deg) translate3d(0, 0, calc(var(--depth) * 0.5));
-      }
-      
-      /* Back face */
-      .cuboid__face:nth-of-type(2) {
-        height: var(--height);
-        width: var(--width);
-        transform: translate(-50%, -50%) rotateX(90deg) rotateZ(180deg) translate3d(0, 0, calc(var(--depth) * 0.5));
-      }
-
-      /* Right face */
-      .cuboid__face:nth-of-type(3) {
-        height: var(--height);
-        width: var(--depth);
-        transform: translate(-50%, -50%) rotateY(90deg) rotateZ(-90deg) translate3d(0, 0, calc(var(--width) * 0.5));
-      }
-
-      /* Left face */
-      .cuboid__face:nth-of-type(4) {
-        height: var(--height);
-        width: var(--depth);
-        transform: translate(-50%, -50%) rotateY(-90deg) rotateZ(90deg) translate3d(0, 0, calc(var(--width) * 0.5));
-      }
-
-      /* Top face */
-      .cuboid__face:nth-of-type(5) {
-        height: var(--depth);
-        width: var(--width);
-        transform: translate(-50%, -50%) translate3d(0, 0, calc(var(--height) * 0.5));
-      }
-
-      /* Bottom face */
-      .cuboid__face:nth-of-type(6) {
-        height: var(--depth);
-        width: var(--width);
-        transform: translate(-50%, -50%) rotateY(180deg) translate3d(0, 0, calc(var(--height) * 0.5));
+        --background: ${texture};
       }
     `;
 
+    const style = document.createElement("link");
+    style.rel = "stylesheet";
+    style.href = "/styles/cuboid.css";
 
     const faces = [];
 
@@ -118,8 +54,8 @@ class Cuboid extends HTMLElement {
     cuboid.classList.add("cuboid");
     cuboid.append(...faces);
 
-    this.shadowRoot.append(style, cuboid);
+    this.shadowRoot.append(cssVariables, style, cuboid);
   }
 }
 
-customElements.define("css-cuboid", Cuboid)
+customElements.define("css-cuboid", Cuboid);
